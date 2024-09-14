696ee73d8fbda5ea969d7b5b2c0e6d0a21f665b4
benjtom <152236555+benjtom@users.noreply.github.com>
2024-09-13 19:21:57 -0600

    add support for various distributions
    now supports uniform and normal

--- various_distributions_stopping.py
+++ various_distributions_stopping.py
@@ -4,15 +4,20 @@ import math
 len_candidates = 100
 num_experiments = 1000
 
-# Function to run many experiments to find the optimal stopping threshold for a uniform distribution. As an infinite number of experiments are performed, the threshold would approach the 'magical' 37%.
-def explore_optimal_thresholds_uniform(len_candidates, num_experiments):
+# Function to run many experiments to find the optimal stopping threshold for the given distribution.
+def explore_optimal_thresholds(distribution, len_candidates, num_experiments):
     optimal_solution_found_count = {}
 
     for i in range(1, len_candidates):                              
         optimal_solution_found_count[str(i)] = 0
 
     for experiment in range(num_experiments):
-        candidates = np.random.uniform(1, 1000, len_candidates)     
+        match distribution:
+            case "uniform":
+                candidates = np.random.uniform(1, 1000, len_candidates)     
+            case "normal":
+                candidates = np.random.normal(50, 10, len_candidates)  
+
         optimal_candidate = max(candidates)                         
 
         for i in range(1, len_candidates):                          
@@ -24,7 +29,7 @@ def explore_optimal_thresholds_uniform(len_candidates, num_experiments):
 
     optimal_candidate_threshold = max(zip(optimal_solution_found_count.values(), optimal_solution_found_count.keys()))[1]       #optimal candidate to stop at according to these completed experiments
     optimal_stopping_threshold = round(float(optimal_candidate_threshold) / len_candidates, 4)                                  #optimal stopping threshold as a percentage
-    print(f"After running {num_experiments} experiments, it was optimal to stop searching through candidates and make the desision after seeing {optimal_stopping_threshold * 100}% of the candidates. In other words, it was optimal to choose a candidate after seeing {optimal_candidate_threshold} out of {len_candidates}.")
+    print(f"After running {num_experiments} experiments with a {distribution} distribution, it was optimal to stop searching through candidates and make the desision after seeing {optimal_stopping_threshold * 100}% of the candidates. In other words, it was optimal to choose a candidate after seeing {optimal_candidate_threshold} out of {len_candidates}.")
 
     return optimal_stopping_threshold
 
@@ -42,20 +47,31 @@ def stopping_algorithm(candidates, threshold):
             return candidates[i]
 
 # Function to run a test on our algorithm.
-def run_algorithm_test_uniform():
-    test_threshold = explore_optimal_thresholds_uniform(len_candidates, num_experiments)            # 'num_experiments' tests are conducted to find an optimal stopping threshold. (We know the optimal threshold is .37 in actuality)
+def run_algorithm_test(distribution):
+    test_threshold = explore_optimal_thresholds(distribution, len_candidates, num_experiments)            # 'num_experiments' tests are conducted to find an optimal stopping threshold. (We know the optimal threshold is .37 usually)
 
-    test_candidates = np.random.uniform(1, 1000, 1000)                                      # a random group of 1000 candidates is generated with a uniform distribution.
+    match distribution:                                                                                   # a random group of 1000 candidates is generated with the given distribution.
+            case "uniform":
+                test_candidates = np.random.uniform(1, 1000, len_candidates)     
+            case "normal":
+                test_candidates = np.random.normal(50, 10, len_candidates)  
+                                   
     test_optimal_candidate = max(test_candidates)                                           # the objectively optimal candidate is recorded, for comparison purposes
 
     test_chosen_candidate = stopping_algorithm(test_candidates, test_threshold)     # Our algorithm is run on the group and it chooses a candidate.
 
-    print(f"After running our algorithm on a test group of size {len_candidates}, using a stopping threshold of {test_threshold}, it chose a candidate with value {test_chosen_candidate}. The best candidate in the group had value {test_optimal_candidate}.")
+    print(f"After running our algorithm on a test group of size {len_candidates} with a {distribution} distribution, using a stopping threshold of {test_threshold}, it chose a candidate with value {test_chosen_candidate}. The best candidate in the group had value {test_optimal_candidate}.")
 
 
+
+# Run the tests using varous distributions.
 
 for i in range(0,2):
-    print(f"\nTest #{i}:")
-    run_algorithm_test_uniform()
+    print(f"\nUniform Test #{i}:")
+    run_algorithm_test("uniform")
+
+for i in range(0,2):
+    print(f"\nNormal Test #{i}:")
+    run_algorithm_test("normal")
 
 
